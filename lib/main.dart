f961ca7e4cf153f7b2b630fd7f89a64ac1360b19
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2022-04-29 20:12:15 +0800

    begin

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
        children: [
          Container(
           width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
+          height: MediaQuery.of(context).size.height * 0.4,
           padding: const EdgeInsets.only(top: 64.0),
           decoration: BoxDecoration(
             color: Colors.blue,
@@ -55,9 +55,10 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             child: Column(
-              children: [
+              children: const [
                 CircleAvatar(
                   radius: 70.0,
+                  backgroundImage: NetworkImage("https://scontent-sin6-1.xx.fbcdn.net/v/t31.18172-8/10557149_1501364603432459_4348081909003327504_o.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHg3h2a41vI1FB7wctZBTmffPYqHEAaT8h89iocQBpPyGmyg02dAvTkbvzciOtO3his41jjef65Obpoh3Uabvq0&_nc_ohc=p34Ls2Hp_wwAX-Am9iV&_nc_ht=scontent-sin6-1.xx&oh=00_AT9US3ePDL2kgWnqTRow7E8faA3TQWZVmiw8iKjDCzR6HQ&oe=6290CB66"),
                 ),
                 Text(
                   'Guntur', 
@@ -68,11 +69,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                  Text(
-                  'Guntur', 
+                  'lecture', 
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.normal,
                   ),
                   
                 ) 
@@ -81,21 +82,50 @@ class _MyHomePageState extends State<MyHomePage> {
             )
           ),
          Container(
-           width: MediaQuery.of(context).size.width,
-           child: Column(
-             children: const [
-               Text('about me'),
-               Text('halo nama saya guntur'),
-               Container(
-                 decoration: BoxDecoration(
-                   border: Border.all(
-                     color: Colors.black,
-                     width: 1.0,
-                  ),
-                 ),
+            width: MediaQuery.of(context).size.width,
+            padding:const EdgeInsets.only(left:50.0, right:50.0),
+            child: Column(
+             children: [
+              const SizedBox(height: 30.0),
+              const Text("about me",
+               style: TextStyle(
+                 fontWeight: FontWeight.bold,
+                 fontSize: 16.0,
                ),
-               Text('skill'),
-           ],
+              ),
+              const SizedBox(height: 20.0),
+              const Text("hi, currently I am lecturing at the Engineering and Informatics Faculty of Universitas Pendidikan Nasional", 
+              textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20.0),
+              Container(
+                margin: const EdgeInsets.only(left: 75.0, right: 75.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                ),
+              ),
+              const SizedBox(height:16.0),
+              const Text("Skills",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+              const SizedBox(height: 16.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text("Angular"),
+                  Text("SQL"),
+                  Text("Flutter"),
+                  Text("English"),
+                ],
+              ),
+              const SizedBox( height: 16.0),
+            ],
            ),
          ), 
         ]
